696d1d2f9199173b4f7fd9de329da7af11fdc9de
Asameshi <asakurashi00@icloud.com>
2025-10-30 03:31:05 +0900

    feat: カードの生成ができるように

--- Assets/Scripts/InGame/Manager/CreateCardManager.cs
+++ Assets/Scripts/InGame/Manager/CreateCardManager.cs
@@ -5,11 +5,20 @@ namespace InGame.Manager
     public class CreateCardManager : MonoBehaviour
     {
         [SerializeField] private GameObject cardPrefab;
-        [SerializeField] private Transform cardsParent;
+        [SerializeField] private Transform playerHandTransform;
 
         private void Start()
         {
-            Instantiate(cardPrefab, cardsParent);
+            CreateCard(playerHandTransform);
+        }
+        
+        /// <summary>
+        /// カードの生成
+        /// </summary>
+        /// <param name="hand"></param>
+        private void CreateCard(Transform hand)
+        {
+            Instantiate(cardPrefab, hand);
         }
     }
 }
--- Assets/Scripts/InGame/Manager/GameManager.cs
+++ Assets/Scripts/InGame/Manager/GameManager.cs
@@ -5,15 +5,6 @@ namespace InGame.Manager
 {
     public class GameManager : MonoBehaviour
     {
-        // 表示・非表示の管理
-        [SerializeField] private GameObject panel;
-        [SerializeField] private TextMeshProUGUI nameText;
         
-        private void Start()
-        {
-            // 初期状態でパネルを非表示にする
-            panel.SetActive(false);
-            nameText.text = "あさめし";
-        }
     }
 }
